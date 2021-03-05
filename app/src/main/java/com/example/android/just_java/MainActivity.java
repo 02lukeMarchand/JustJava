5ca3470067c98e5effac435ddef5b038823afcaa
Luke <02luke.marchand@gulllakecs.org>
2021-03-05 12:38:40 -0500

    Updated display method to displayQuantity

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void increment(View view){
         orders++;
-        display(orders);
+        displayQuantity(orders);
     }
 
     public void decrement(View view){
@@ -41,13 +41,13 @@ public class MainActivity extends AppCompatActivity {
         if (orders <= 0){
             orders = 0;
         }
-        display(orders);
+        displayQuantity(orders);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
